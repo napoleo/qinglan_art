9cd19750692ca67384ef7944fc9bb3aa72639018
Mark <wertyuiopsdfghjkl@163.com>
2016-12-04 20:28:06 +0800

    No more comment

--- front-end/category.js
+++ front-end/category.js
@@ -1,3 +1,8 @@
-function toActive(obj) {
+function closeAll() {
+	document.getElementById("instrument").setAttribute("class","collapse nav nav-pills sub-categor");
+	document.getElementById("vocal").setAttribute("class","collapse nav nav-pills sub-categor");
+	document.getElementById("drawing").setAttribute("class","collapse nav nav-pills sub-categor");
+	document.getElementById("calligraphy").setAttribute("class","collapse nav nav-pills sub-categor");
+	document.getElementById("chess").setAttribute("class","collapse nav nav-pills sub-categor");
 
 }
