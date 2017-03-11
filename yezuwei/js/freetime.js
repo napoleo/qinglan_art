ff736a918d60734cee52a6e78ec7ed490b07f3b1
Mark Yeh <molokai@qq.com>
2017-03-11 20:27:03 +0800

    每个页面的导航功能全部加上,除了课程购买页面

--- yezuwei/js/freetime.js
+++ yezuwei/js/freetime.js
@@ -1,9 +1,9 @@
 $(function() {
-	$("td").bind("click",function(ev) {
-		$(ev.target).find("span").toggleClass("fa fa-check-circle");
+	$(".free-time-search td").bind("click",function(ev) {
+		$(ev.target).find("span").toggleClass("fa fa-check-circle green");
 	})
 	$(".free-time-search span").bind("click",function(ev) {
-		$(ev.target).toggleClass("fa fa-check-circle");
+		$(ev.target).toggleClass("fa fa-check-circle green");
 	})
 
 });
