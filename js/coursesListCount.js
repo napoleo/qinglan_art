9d040a08543ee0c94be1131c0c1bbb5145858dac
Mark Yeh <molokai@qq.com>
2017-03-11 19:14:35 +0800

    重新开始

--- js/coursesListCount.js
+++
@@ -1,8 +0,0 @@
-window.onload=init;
-function init(){
-	var x=document.getElementsByClassName("courses");
-	$('course-count').innerHTML=x.length;	
-}
-function $(id){
-	return document.getElementById(id);
-}
